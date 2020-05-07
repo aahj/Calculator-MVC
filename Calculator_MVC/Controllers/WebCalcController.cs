d7f667ae2ca4da2312037eddacfd9aeed5696cdd
aahj <aamiralihussain53@gamil.com>
2020-05-07 06:20:26 +0500

    Add project files.

---
+++ Calculator_MVC/Controllers/WebCalcController.cs
@@ -0,0 +1,39 @@
+﻿using Calculator_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calculator_MVC.Controllers
+{
+    public class WebCalcController : Controller
+    {
+        // GET: WebCalc
+        public ActionResult Index()
+        {
+            return View(new web_based_calc());
+        }
+        [HttpPost]
+        public ActionResult Index(web_based_calc c,string calculate)
+        {
+            if (calculate == "add")
+            {
+                c.tot = c.input1 + c.input2;
+            }
+            else if (calculate == "sub")
+            {
+                c.tot = c.input1 - c.input2;
+            }
+            else if (calculate == "mul")
+            {
+                c.tot = c.input1 * c.input2;
+            }
+            else
+            {
+                c.tot = c.input1 / c.input2;
+            }
+            return View(c);
+        }
+    }
+}
---
+++ Calculator_MVC/Models/web_based_calc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator_MVC.Models
+{
+    public class web_based_calc
+    {
+        public int input1 { get; set; }
+        public int input2 { get; set; }
+        public double tot { get; set; }
+    }
+}
